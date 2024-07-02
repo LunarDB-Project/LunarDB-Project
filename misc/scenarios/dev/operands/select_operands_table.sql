dcc3061e4226a76bbeb9d5b75b81b1cbe71bfadc
TheAncientOwl <dele.alex.o.o@gmail.com>
2024-07-02 21:33:52 +0300

    Update moonlight dev scenarios

---
+++ misc/scenarios/dev/operands/select_operands_table.sql
@@ -0,0 +1,20 @@
+database drop test_db;
+database create test_db;
+database use test_db;
+schema table_schema01 { field1: String; field2: Integer; };
+schema table_schema02 { str: String; obj: table_schema01; };
+create table somedocument based on table_schema02;
+insert into somedocument objects [  
+  { "str": "val", "obj": { "field1": "val11", "field2": "10" } }  
+  { "str": "val101", "obj": { "field1": "val31", "field2": "15" } }  
+  { "str": "val202", "obj": { "field1": "val22", "field2": "20" } }
+];
+select from structure somedocument where ( str like val. ) fields [ _rid, str, obj, obj.field1, obj.field2 ];
+select from structure somedocument where ( str like val.+ ) fields [ _rid, str, obj, obj.field1, obj.field2 ];
+select from structure somedocument where ( str like val.* ) fields [ _rid, str, obj, obj.field1, obj.field2 ];
+select from structure somedocument where ( str like \w* ) fields [ _rid, str, obj, obj.field1, obj.field2 ];
+select from structure somedocument where ( str like \w* and obj.field1 like val.1 ) fields [ _rid, str, obj, obj.field1, obj.field2 ];
+
+select from structure somedocument where ( str in [ val101, val202 ] ) fields [ _rid, str, obj, obj.field1, obj.field2 ];
+
+select from structure somedocument where ( obj.field2 between 10 and 15 ) fields [ _rid, str, obj, obj.field1, obj.field2 ];
---
+++ misc/scenarios/dev/rollback/document/rollback_document_complex_commits.sql
@@ -0,0 +1,41 @@
+database drop test_db;
+database create test_db;
+database use test_db;
+schema document_schema01 { field1: String; field2: Integer; };
+schema document_schema02 { str: String; obj: document_schema01; };
+
+commit;
+create collection somedocument_test based on document_schema02;
+select from structure somedocument_test where ( 1 == 1 ) fields [ _rid, str, obj.field1, obj.field2 ];
+
+commit;
+insert into somedocument_test objects [  
+  { "str": "val11", "obj": { "field1": "val11", "field2": "11" } }  
+  { "str": "val111", "obj": { "field1": "val111", "field2": "111" } }
+  { "str": "val111", "obj": { "field1": "val111", "field2": "111" } }
+  { "str": "val99", "obj": { "field1": "val99", "field2": "99" } }  
+];
+select from structure somedocument_test where ( 1 == 1 ) fields [ _rid, str, obj.field1, obj.field2 ];
+
+commit;
+update structure somedocument_test where ( str == val111 ) modify [ str => val222, obj.field1 => val222, obj.field2 => 222 ];
+select from structure somedocument_test where ( 1 == 1 ) fields [ _rid, str, obj.field1, obj.field2 ];
+
+commit;
+update structure somedocument_test where ( obj.field1 == val222 ) modify [ str => val333, obj.field1 => val333, obj.field2 => 333 ];
+select from structure somedocument_test where ( 1 == 1 ) fields [ _rid, str, obj.field1, obj.field2 ];
+
+rollback;
+select from structure somedocument_test where ( 1 == 1 ) fields [ _rid, str, obj.field1, obj.field2 ];
+
+rollback;
+select from structure somedocument_test where ( 1 == 1 ) fields [ _rid, str, obj.field1, obj.field2 ];
+
+rollback;
+select from structure somedocument_test where ( 1 == 1 ) fields [ _rid, str, obj.field1, obj.field2 ];
+
+rollback;
+select from structure somedocument_test where ( 1 == 1 ) fields [ _rid, str, obj.field1, obj.field2 ];
+
+rollback;
+select from structure somedocument_test where ( 1 == 1 ) fields [ _rid, str, obj.field1, obj.field2 ];
