01abbc3ccab37b7fe15df9386c8bc56094f6f6f6
TheAncientOwl <dele.alex.o.o@gmail.com>
2024-07-03 04:25:08 +0300

    Revert "Implement Astral::CreateExecutor multiple collections from schemas list"
    This reverts commit 4a1bc55e30eff883b5c1e238f9ccb1da779ea89c.

--- src/Selenity/src/Managers/DatabaseManager.cpp
+++ src/Selenity/src/Managers/DatabaseManager.cpp
@@ -71,14 +71,11 @@ void DatabaseManager::createCollection(
         }
         catch (std::exception const& e)
         {
-            if (collection_type == LunarDB::Common::QueryData::Primitives::EStructureType::Table)
-            {
-                auto new_collection_name{name + "_" + field.name};
+            auto new_collection_name{name + "_" + field.name};
 
-                createCollection(new_collection_name, field.type, collection_type, {});
+            createCollection(new_collection_name, field.type, collection_type, {});
 
-                updated_bindings.emplace_back(field.name, std::move(new_collection_name));
-            }
+            updated_bindings.emplace_back(field.name, std::move(new_collection_name));
         }
     }
 
